40e67f51a18992ba663e984bd0ad86648dca2466
jairo-serratoo <43346832+jairo-serratoo@users.noreply.github.com>
2020-06-17 20:03:37 -0500

    se optimizo el programa uso de basico de metodos sesion 2

--- src/sesion1_comentarios/Comentarios.java
+++ src/sesion1_comentarios/Comentarios.java
@@ -14,23 +14,45 @@ public class Comentarios {
     //Metodo Main
     public static void main(String[] args){
         //Atributos
-        String name = "Jairo";
-        int edad = 28;
-        double peso1 = 115.9;
-        double peso2 = restePeso(peso1);
-        System.out.println(name);
-        System.out.println(edad);
-        System.out.println(peso2);
+        String nameJairo = "Jairo";
+        String nameLuis = "Luis";
+        String nameOmar = "Omar";
+        String nameCarlos = "Carlos";
+        String nameFernando= "Fernando";
+        //int edad = 28;
+        double pesoJairo = 115.9;
+        double pesoLuis = 150;
+        double pesoOmar = 98;
+        double pesoCarlos = 60;
+        double pesoFernando = 49;
+        
+        double pesoDeseadoJairo = restePeso(pesoJairo, 20);
+        double pesoDeseadoLuis = restePeso(pesoLuis, 30);
+        double pesoDeseadoOmar = restePeso(pesoOmar, 10);
+        double pesoDeseadoCarlos = restePeso(pesoCarlos, 5);
+        double pesoDeseadoFernando = restePeso(pesoFernando, 5.5); //Ganar
+        System.out.println(nameJairo);
+        System.out.println(nameLuis);
+        System.out.println(nameOmar);
+        System.out.println(nameCarlos);
+        System.out.println(nameFernando);
+        //System.out.println(edad);
+        System.out.println(pesoDeseadoJairo);
+        System.out.println(pesoDeseadoLuis);
+        System.out.println(pesoDeseadoOmar);
+        System.out.println(pesoDeseadoCarlos);
+        System.out.println(pesoDeseadoFernando);
     }
     
     /**
      * Este metodo se creo a raiz de que Jairo es vanidoso y se queria quitar 20 kilos de encima
      * @param peso el peso real del vanidoso de Jairo
-     * @return el peso real menos 20 kilos
+     * @param pesoAPerder es el peso que se le restara
+     * @return el peso real menos el peso a perder
      */
-    public static Double restePeso(double peso){
+    public static Double restePeso(double peso, double pesoAPerder){
         double nuevoPeso = 0.0;
-        nuevoPeso = peso - 20;
+        nuevoPeso = peso - pesoAPerder;
         return nuevoPeso;
     }
     
@@ -52,5 +74,9 @@ public class Comentarios {
             Omar pesa 98 kg, pero se quiere quitar 10 kilos
             Carlos pesa 60 kg, pero se quiere quitar 5 kilos
             Fernando pesa 49 kilos y quiere aumentar 5.5 kilos
+    
+        tarea 18 Junio
+    -Lograr que el metodo que vamos trabajando, pueda sumarle el peso deseado de Fernando
+    -Reducir el codigo a menos lineas (Tema: concatenacion)
     */
 }
